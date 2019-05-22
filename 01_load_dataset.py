d43995dfd4292b212efe4e687050fcd3e8a6081f
Jonas Arilho <jonasarilho@gmail.com>
2019-05-21 22:49:52 -0300

    saving arrays to .npy files

--- 01_load_dataset.py
+++ 01_load_dataset.py
@@ -1,25 +1,31 @@
 import numpy as np
 # import matplotlib
+import glob
 # import matplotlib.pyplot as plt
 from astropy.io import fits
 
 file_path = "F:/merging/fits/"
-file_name = "merger_test_587722982838436141"
+out_path = "F:/merging/npy/"
+ex_file_name = "merger_test_587722982838436141"
 
-bands = ["g", "i", "r", "u", "z"]
-data = {}
+list_files = glob.glob(file_path + '*_g.fit.gz')
 
-for band in bands:
-    file = file_path + file_name + "_" + band + ".fit.gz"
-    hdul = fits.open(file, memmap=False)
-    hdul.verify('fix')
-    hdul.info()
+for file in list_files[:10]:
+    data = {}
+    bands = ["g", "i", "r", "u", "z"]
+    for band in bands:
+        fits_file = file[:-9] + "_" + band + ".fit.gz"
+        hdul = fits.open(fits_file, memmap=False)
+        hdul.verify('fix')
+        # hdul.info()
 
-    # print(hdul[0].header)
-    data[band] = hdul[0].data
-    hdul.close()
+        # print(hdul[0].header)
+        data[band] = hdul[0].data
+        hdul.close()
 
-image_data = np.array([data["g"], data["i"], data["r"], data["u"], data["z"]])
+    image = np.array([data["g"], data["i"], data["r"], data["u"], data["z"]])
 
-print(type(image_data))
-print(image_data.shape)
+    file_name = file[len(file_path):-9]
+    np.save(out_path + file_name, image)
+    # print(type(image))
+    # print(image.shape)
