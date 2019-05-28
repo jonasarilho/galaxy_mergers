245f773aede5ca0015ad216a0ad43fe4d018a916
Jonas Arilho <jonasarilho@gmail.com>
2019-05-28 19:42:43 -0300

    resizing and saving image

--- 02_resize_images.py
+++ 02_resize_images.py
@@ -3,9 +3,11 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from skimage.transform import resize
+from astropy.visualization import LogStretch
+from astropy.visualization.mpl_normalize import ImageNormalize
 
 
-def resize_img(tensor, cfg):
+def resize_img(tensor, cfg, size):
     bands_id = {"g": 0, "i": 1, "r": 2, "u": 3, "z": 4}
 
     tensors_list = []
@@ -15,29 +17,37 @@ def resize_img(tensor, cfg):
 
     img = np.array(tensors_list)
     image = np.transpose(img, (1, 2, 0))
-    image = image / 65536
-    resize(image, (32, 32), preserve_range=True)
+    image = (image / 65536).astype('float32')
+    image = resize(image, (size, size), preserve_range=True)
     print(image.shape)
     return image
 
 
 def main():
     img_path = sys.argv[1]
-    if len(sys.argv) == 3:
+    if len(sys.argv) > 2:
         cfg = sys.argv[2]
 
     else:
         cfg = "rgu"
 
+    if len(sys.argv) > 3:
+        size = int(sys.argv[3])
+
+    else:
+        size = 32
+
     dataframe = pd.read_csv("dataframe.csv")
     for index, data in dataframe.iterrows():
         tensor = np.load(data["file"])
-        img = resize_img(tensor, cfg)
+        img = resize_img(tensor, cfg, size)
         file = img_path + str(data["objid"])
+        norm = ImageNormalize(stretch=LogStretch())
         np.save(file, img)
-        plt.imshow(img)
-        plt.show()
-        break
+        plt.imshow(img, norm=norm)
+        plt.savefig("img" + str(index) + ".png")
+        if index == 5:
+            break
 
 
 if __name__ == '__main__':
--- run.sh
+++ run.sh
@@ -3,10 +3,11 @@ FITS_PATH="F:/merging/fits/"
 NPY_PATH="F:/merging/npy/"
 IMG_PATH="F:/merging/img/"
 BANDS_CFG="rgu"
+IMG_SIZE="32"
 
 if [ $1 eq "1" ]
 then
     python 01_load_dataset.py $FITS_PATH $NPY_PATH
 else
-    python 02_resize_images.py $IMG_PATH $BANDS_CFG
+    python 02_resize_images.py $IMG_PATH $BANDS_CFG $IMG_SIZE
 fi
