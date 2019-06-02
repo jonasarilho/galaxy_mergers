bd11196ec106865772ea5d8c40625103ade8da49
Jonas Arilho <jonasarilho@gmail.com>
2019-06-02 16:00:52 -0300

    improving resize time

--- 02_resize_images.py
+++ 02_resize_images.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import numpy as np
 import pandas as pd
@@ -5,15 +6,17 @@ from skimage.transform import resize
 from astropy.visualization import LogStretch
 
 
-def resize_img(tensor, cfg, size):
+def bands_list(cfg):
     bands_id = {"g": 0, "i": 1, "r": 2, "u": 3, "z": 4}
-
-    tensors_list = []
+    ids_list = []
     for band in cfg:
-        id = bands_id[band]
-        tensors_list.append(tensor[id, :, :])
+        ids_list.append(bands_id[band])
 
-    img = np.array(tensors_list)
+    return ids_list
+
+
+def resize_img(tensor, bands, size):
+    img = tensor[bands, :, :]
     image = np.transpose(img, (1, 2, 0))
     image = (image / 65536).astype('float32')
     image = resize(image, (size, size), preserve_range=True)
@@ -24,6 +27,9 @@ def resize_img(tensor, cfg, size):
 
 def main():
     img_path = sys.argv[1]
+    if not os.path.exists(img_path):
+        os.makedirs(img_path)
+
     if len(sys.argv) > 2:
         cfg = sys.argv[2]
 
@@ -39,9 +45,13 @@ def main():
     dataframe = pd.read_csv("dataframe.csv")
     for index, data in dataframe.iterrows():
         tensor = np.load(data["file"])
-        img = resize_img(tensor, cfg, size)
+        bands = bands_list(cfg)
+        img = resize_img(tensor, bands, size)
         file = img_path + str(data["objid"])
         np.save(file, img)
+        if index % 500 == 0:
+            msg = "Already saved {} files with size {}x{}"
+            print(msg.format(index, size, size))
 
 
 if __name__ == '__main__':
