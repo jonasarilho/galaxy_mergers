05c0072a0b5af32f28953cf0ee316d5e9dffc043
Jonas Arilho <jonasarilho@gmail.com>
2019-06-02 18:32:11 -0300

    separating test set and planning vgg cnn experiment

---
+++ Experiments/01_VGG_CNN.py
@@ -0,0 +1,30 @@
+# reference:
+# https://gist.github.com/fchollet/0830affa1f7f19fd47b06d4cf89ed44d
+# VGG-Style CNN
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
--- run.sh
+++ run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 FITS_PATH="F:/merging/fits/"
 NPY_PATH="F:/merging/npy/"
-IMG_PATH="F:/merging/img/"
+IMG_PATH="F:/merging/img32/"
 BANDS_CFG="rgu"
 IMG_SIZE="32"
 
 if [ $1 eq "1" ]
 then
     python 01_load_dataset.py $FITS_PATH $NPY_PATH
-else
+elif [ $1 eq "2" ]
+then
     python 02_resize_images.py $IMG_PATH $BANDS_CFG $IMG_SIZE
+elif [ $1 eq "3" ]
+then
+    python 03_separate_test_set.py $IMG_PATH $IMG_SIZE
+else
+    python 01_load_dataset.py $FITS_PATH $NPY_PATH
+    python 02_resize_images.py $IMG_PATH $BANDS_CFG $IMG_SIZE
+    python 03_separate_test_set.py $IMG_PATH
 fi
